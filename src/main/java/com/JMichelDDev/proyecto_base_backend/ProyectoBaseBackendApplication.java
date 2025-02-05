e1492a6d946a9906dabd20f9cb0d0447944d0dad
JhonMichelDiazDev <jhondiaz266@gmail.com>
2025-02-05 08:18:08 -0500

    Inicia proyecto

---
+++ src/main/java/com/JMichelDDev/proyecto_base_backend/ProyectoBaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.JMichelDDev.proyecto_base_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JMichelDDev/proyecto_base_backend/ProyectoBaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JMichelDDev.proyecto_base_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
